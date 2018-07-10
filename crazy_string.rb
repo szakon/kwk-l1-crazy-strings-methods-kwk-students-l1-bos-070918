05343cb201e892cbc0787aa3692308d7d7ec1ee5
szakon <sarahzakon@gmail.com>
2018-07-10 17:18:51 +0000

    automatically backed up by learn

--- crazy_string.rb
+++ crazy_string.rb
@@ -1,8 +1,8 @@
 # write your method here
 
 def crazy_strings("Hello", "Friends")
-a ="Hello".upcase.reverse
-b = "Friends".gsub("s","z").swapcase
+a = #{Hello}".upcase.reverse
+b = #{Friends}.gsub("s","z").swapcase
 puts a + b
 end
 puts crazy_strings
